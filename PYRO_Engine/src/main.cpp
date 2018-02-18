48aa83425ba871d4ef092094f76021dd22ba4821
EZ stuff <36305432+EZ-code-102@users.noreply.github.com>
2018-02-17 23:00:57 -0600

    ADD function LOG_GET_BUILD_MODE
    added the function LOG_GET_BUILD_MODE and the enum BUILD_MODE

--- PYRO_Engine/src/main.cpp
+++ PYRO_Engine/src/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
+enum BUILD_MODE {
+	DEBUG_MODE = 0, RELEASE_MODE = 1
+};
+
+void LOG_GET_BUILD_MODE(BUILD_MODE* mode)
+{
+*mode = BUILD_MODE::RELEASE_MODE;
+#ifdef _DEBUG
+	*mode = BUILD_MODE::DEBUG_MODE;
+#endif //DEBUG
+}
+
 
 int main(void)
 {
 	std::cout << "Hello GitHub!" << std::endl;
+
+	std::cout << std::endl;
+
+	BUILD_MODE currentMode;
+
+	LOG_GET_BUILD_MODE(&currentMode);
+	if (currentMode == BUILD_MODE::DEBUG_MODE)
+		std::cout << "Current build mode is debug" << std::endl;
+	else
+		std::cout << "Current build mode is release" << std::endl;
 
 	int x = 0;
 	std::cin >> x;
